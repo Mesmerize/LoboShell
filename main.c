e5f888fdd6afdc32d71dabefe33294fc43aa7658
Brandon Luna Rodriguez <disrespectings@gmail.com>
2022-04-03 00:26:30 -0700

    Project Done, Need to Refactor / add Comments

--- main.c
+++ main.c
@@ -9,75 +9,26 @@
 #include "constants.h"
 #include "parsetools.h"
 
-void syserror(const char *);
-
 int main()
 {
     // Buffer for reading one line of input
     char line[MAX_LINE_CHARS];
     // holds separated words based on whitespace
-    char *line_words[MAX_LINE_WORDS + 1];
+    char* lineWords[MAX_LINE_WORDS + 1];
 
+    // Shell Prompt
     printf("Welcome to SegFault's simple shell\npress CTRL + D to exit\n>");
 
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
-    while (fgets(line, MAX_LINE_CHARS, stdin))
-    {
-        int num_words = split_cmd_line(line, line_words);
+    while( fgets(line, MAX_LINE_CHARS, stdin) ) {
 
-        for (int i = 0; i < num_words; i++)
-        {
-            printf("%s\n", line_words[i]);
-        }
+        // Gets the number of words user entered
+        int numWords = split_cmd_line(line, lineWords);
+
+        // Retrieves commands from user input and executes them
+        execute(lineWords, numWords);
     }
-
-    printf("Executing Command!\n");
-
-    // testing pipe execution
-    int pfd[2];
-    pid_t pid, ret;
-
-    if (pipe(pfd) == -1)
-    {
-        syserror("Could not create a pipe");
-        return -1;
-    }
-
-    pid = fork();
-
-    if (pid < 0)
-    {
-        syserror("Fork Failed!");
-        return 1;
-    }
-
-    if (pid == 0)
-    {
-        printf("%d: I'm the child\n", getpid());
-        fflush(stdout);
-        dup(pfd[1]); // Writing into STDIN, left side of pipe
-        close(pfd[1]);
-        close(pfd[0]);
-        execvp(line_words[0], line_words);
-        perror("exec must have failed");
-    }
-
-    // Killing all kids
-    close(pfd[1]);
-    close(pfd[0]);
-
-    pid = waitpid(-1, &ret, 0);
-    // printf( "%d: parent done\n", getpid());
-    // printf( "%d: child %d returned %d\n", getpid(), pid, WEXITSTATUS(ret));
 
     return 0;
-}
-
-void syserror(const char *s)
-{
-    extern int errno;
-    fprintf(stderr, "%s\n", s);
-    fprintf(stderr, " (%s)\n", strerror(errno));
-    exit(1);
 }
--- src/parsetools.c
+++ src/parsetools.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include "constants.h"
 #include "parsetools.h"
 
@@ -21,4 +30,186 @@ int split_cmd_line(char* line, char** list_to_populate) {
    };
 
    return i;
+}
+
+void syserror(const char* s) {
+    extern int errno;
+    fprintf( stderr, "%s\n", s );
+    fprintf( stderr, " (%s)\n", strerror(errno) );
+    exit( 1 );
+}
+
+int isLessOp(const char* token) {
+    // If token matches '<' operator then return true (1) 
+    if(strcmp(token, "<") == 0) {
+        return 1;
+    } return 0; // Else false (0)
+}
+
+int isPipeOp(const char* token) {
+    // If token matches '|' operator then return true (1) 
+    if(strcmp(token, "|") == 0) {
+        return 1;
+    } return 0; // Else false (0)
+}
+
+int isGreaterOp(const char* token) {
+    // If token matches '>' operator then return true (1) 
+    if(strcmp(token, ">") == 0) {
+        return 1;
+    } return 0; // Else false (0)
+}
+
+int isAppendOp(const char* token) {
+    // If token matches '>>' operator then return true (1) 
+    if(strcmp(token, ">>") == 0) {
+        return 1;
+    } return 0; // Else false (0)
+}
+
+int isValidCommand(const char* token) {
+    // If '<' or '>' or '|' is detected then return false (0) since it is not a command
+    if(isLessOp(token) == 1 || isGreaterOp(token) == 1 || isPipeOp(token) == 1) {
+        return 0;
+    }   return 1;   // else return true (1)
+}
+
+void execute(char* commandLine[], int numTokens) {
+    // Init Local Vars
+    int fileValidator, ioValidator, commandCount, numPipes, firstCommand, args, append;
+    append = fileValidator = ioValidator = commandCount = numPipes = 0;
+    firstCommand = args = 1;
+
+    // For loop traversing through commands
+    for(int i  = 0; i < numTokens; i++) {
+        // If pipe is detected then we increment number of pipes
+        if(isPipeOp(commandLine[i]) == 1) {
+            numPipes++;
+        }
+    }
+
+    // Allocating memory to commandList (instance of struct)
+    command_info* commandList = malloc((numPipes + 1) * sizeof(command_info));
+    // Allocating memory to commandList index of args
+    commandList[commandCount].args = malloc(numTokens * sizeof(char*));
+
+    for(int i = 0; i < numTokens; i++) {
+
+        if(firstCommand == 0) {
+            if(isValidCommand(commandLine[i]) == 1) {
+                if(fileValidator == 1) {
+                    if (ioValidator == 0) {
+                        strcpy((commandList[commandCount].outFile = malloc(strlen(commandLine[i]) + 1)), commandLine[i]);
+                    }
+                    else {
+                        strcpy((commandList[commandCount].inFile = malloc(strlen(commandLine[i]) + 1)), commandLine[i]);
+                    }
+                }
+                else {
+                    strcpy((commandList[commandCount].args[args++] = malloc(strlen(commandLine[i]) + 1)), commandLine[i]);
+                    commandList[commandCount].counter++;
+                }
+            }
+            else if (isLessOp(commandLine[i]) == 1) {
+                fileValidator = ioValidator = commandList[commandCount].inputFlag = 1;
+            }
+            else if (isGreaterOp(commandLine[i]) == 1) {
+                fileValidator = commandList[commandCount].outputFlag = 1;
+                ioValidator = 0;
+            }
+            else if (isAppendOp(commandLine[i]) == 1) {
+                fileValidator = commandList[commandCount].append = 1;
+                ioValidator = 0;
+            }
+            else if (isPipeOp(commandLine[i]) == 1) {
+                commandList[++commandCount].args = malloc(numTokens * sizeof(char*));
+                firstCommand = 1;
+            }
+        }
+
+        else {
+            if(isValidCommand(commandLine[i])){
+                strcpy((commandList[commandCount].indent = malloc(strlen(commandLine[i]) + 1)), commandLine[i]);
+                strcpy((commandList[commandCount].args[0] = malloc(strlen(commandLine[i]) + 1)), commandLine[i]);
+                commandList[commandCount].counter = args = 1;
+                commandList[commandCount].inputFlag = commandList[commandCount].outputFlag = fileValidator = firstCommand = 0;
+            }
+            else if(isPipeOp(commandLine[i])){
+                commandList[++commandCount].args = malloc(sizeof(char*));
+                firstCommand = 1;
+            }
+        }
+
+    }
+
+    int pfd[numPipes * 2];
+    for (int i = 0; i < numPipes; i++){
+        if (pipe(pfd + i * 2) < 0)
+            syserror( "Error creating pipes" );
+    }
+
+    for (int i = 0; i < numPipes+1; i++) {
+        commandList[i].args[commandList[i].counter] = NULL;
+        int numWords = commandList[i].counter;
+        for (int j = 0; j < numWords; j++) {
+            if (commandList[i].args[j][0] == '\"' || commandList[i].args[j][0] == '\''){
+                commandList[i].args[j]++;
+                commandList[i].args[j][(strlen(commandList[i].args[j]-1)) - 2] = 0;
+            }
+        }
+
+        int pid = fork();
+        if(pid == -1){
+            syserror( "Error creating fork" );
+        }
+
+        else if(pid == 0){
+            if (commandList[i].outputFlag) {
+                int fd;
+                if ((fd = open(commandList[i].outFile, O_WRONLY | O_CREAT | O_TRUNC, 0777)) < 0)
+                    syserror("Error opening output file");
+                if (dup2(fd, STDOUT_FILENO) < 0)
+                    syserror("Error connecting STDOUT to file descriptor");
+            }
+
+            if (commandList[i].append) {
+                int fd;
+                if ((fd = open(commandList[i].outFile, O_WRONLY | O_APPEND, 0777)) < 0)
+                    syserror("Error opening output file");
+                if (dup2(fd, STDOUT_FILENO) < 0)
+                    syserror("Error connecting STDOUT to file descriptor");
+            }
+
+            if (commandList[i].inputFlag) {
+                int fd;
+                if ((fd = open(commandList[i].inFile, O_RDONLY)) < 0)
+                    syserror("Error opening input file");
+                if (dup2(fd, STDIN_FILENO) < 0)
+                    syserror("Error connecting STDIN to file descriptor");
+            }
+            if (i != 0)
+                if (dup2(pfd[(i-1)*2], STDIN_FILENO) < 0)
+                    syserror( "Error connecting STDIN to pipe read pipe" );
+            if (i != numPipes)
+                if (dup2(pfd[(i*2)+1], STDOUT_FILENO) < 0)
+                    syserror( "Error connecting STDOUT to write pipe" );
+            for (int i = 0; i < (numPipes) * 2; i ++)
+                if (close(pfd[i]) < 0)
+                    syserror( "Error closing pipes" );
+
+            execvp(commandList[i].indent, commandList[i].args);
+
+            syserror( "Error could not execvp" );
+
+        }
+
+    } // end of for loop
+
+    for (int i = 0; i < (numPipes) * 2; i ++)
+        if (close(pfd[i]) < 0)
+            syserror( "Error closing remainder pipes" );
+
+    while ( wait(NULL) != -1)
+        ;
+
 }
